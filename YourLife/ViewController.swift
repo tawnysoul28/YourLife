ab592ff9d2cc5d25a2553a473747e015c7bcf155
Bob <tawnysoul@MacBook-Pro-28.local>
2019-11-28 18:47:14 +0700

    first commit

---
+++ YourLife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YourLife
+//
+//  Created by Bob on 28/11/2019.
+//  Copyright © 2019 Bob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
